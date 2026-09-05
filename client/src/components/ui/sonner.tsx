d1ac954c8f0d691e6579eeea2181ba84ee7c7146
goodylove <goodyc474@gmail.com>
2026-09-05 10:04:05 +0100

    fix sonner

--- client/src/components/ui/sonner.tsx
+++ client/src/components/ui/sonner.tsx
@@ -2,5 +2,23 @@
 import { Toaster as SonnerToaster, type ToasterProps } from "sonner";
 
 export function Toaster(props: ToasterProps) {
-  return <SonnerToaster position="top-center" {...props} />;
+  return (
+    <SonnerToaster
+      closeButton
+      duration={4000}
+      position="top-center"
+      toastOptions={{
+        classNames: {
+          toast:
+            "!rounded-xl !border-[var(--marketing-action)]/25 !bg-[var(--marketing-action-soft)] !text-foreground ",
+          title: "!text-sm !font-bold !text-[var(--marketing-action)]",
+          description: "!text-xs !text-foreground/70",
+          icon: "!text-[var(--marketing-action)]",
+          closeButton:
+            "!border-[var(--marketing-action)]/20 !bg-white !text-[var(--marketing-action)] hover:!bg-[var(--marketing-action-soft)]",
+        },
+      }}
+      {...props}
+    />
+  );
 }
